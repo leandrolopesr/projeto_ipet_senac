8d39221dcf3c956579706f4e62c3e3da5b543eec
Thiago Ferreira <111807730+TFbr97@users.noreply.github.com>
2023-01-20 19:07:01 -0300

    Criação do main Adotar pet
    Criação do modal na página do adotar pet

---
+++ scripts/modal.js
@@ -0,0 +1,216 @@
+// pegue o botão e o modal
+var openModal = document.getElementById("open-modal");
+var modal = document.getElementById("modal");
+
+// pegue o botão de fechar
+var closeModal = document.getElementsByClassName("close-modal")[0];
+
+// abra o modal ao clicar no botão
+openModal.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // feche o modal ao clicar no botão de fechar
+  closeModal.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // feche o modal ao clicar fora dele
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  
+  // pegue o botão e o modal2
+var openModal = document.getElementById("open-modal2");
+var modal = document.getElementById("modal");
+
+// pegue o botão de fechar
+var closeModal = document.getElementsByClassName("close-modal")[0];
+
+// abra o modal ao clicar no botão
+openModal.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // feche o modal ao clicar no botão de fechar
+  closeModal.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // feche o modal ao clicar fora dele
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  
+  // pegue o botão e o modal3
+var openModal = document.getElementById("open-modal3");
+var modal = document.getElementById("modal");
+
+// pegue o botão de fechar
+var closeModal = document.getElementsByClassName("close-modal")[0];
+
+// abra o modal ao clicar no botão
+openModal.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // feche o modal ao clicar no botão de fechar
+  closeModal.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // feche o modal ao clicar fora dele
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  
+  // pegue o botão e o modal4
+var openModal = document.getElementById("open-modal4");
+var modal = document.getElementById("modal");
+
+// pegue o botão de fechar
+var closeModal = document.getElementsByClassName("close-modal")[0];
+
+// abra o modal ao clicar no botão
+openModal.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // feche o modal ao clicar no botão de fechar
+  closeModal.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // feche o modal ao clicar fora dele
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+    // pegue o botão e o modal5
+var openModal = document.getElementById("open-modal5");
+var modal = document.getElementById("modal");
+
+// pegue o botão de fechar
+var closeModal = document.getElementsByClassName("close-modal")[0];
+
+// abra o modal ao clicar no botão
+openModal.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // feche o modal ao clicar no botão de fechar
+  closeModal.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // feche o modal ao clicar fora dele
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+    // pegue o botão e o modal6
+var openModal = document.getElementById("open-modal6");
+var modal = document.getElementById("modal");
+
+// pegue o botão de fechar
+var closeModal = document.getElementsByClassName("close-modal")[0];
+
+// abra o modal ao clicar no botão
+openModal.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // feche o modal ao clicar no botão de fechar
+  closeModal.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // feche o modal ao clicar fora dele
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+    // pegue o botão e o modal7
+var openModal = document.getElementById("open-modal7");
+var modal = document.getElementById("modal");
+
+// pegue o botão de fechar
+var closeModal = document.getElementsByClassName("close-modal")[0];
+
+// abra o modal ao clicar no botão
+openModal.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // feche o modal ao clicar no botão de fechar
+  closeModal.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // feche o modal ao clicar fora dele
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+    // pegue o botão e o modal8
+var openModal = document.getElementById("open-modal8");
+var modal = document.getElementById("modal");
+
+// pegue o botão de fechar
+var closeModal = document.getElementsByClassName("close-modal")[0];
+
+// abra o modal ao clicar no botão
+openModal.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // feche o modal ao clicar no botão de fechar
+  closeModal.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // feche o modal ao clicar fora dele
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+    // pegue o botão e o modal9
+var openModal = document.getElementById("open-modal9");
+var modal = document.getElementById("modal");
+
+// pegue o botão de fechar
+var closeModal = document.getElementsByClassName("close-modal")[0];
+
+// abra o modal ao clicar no botão
+openModal.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // feche o modal ao clicar no botão de fechar
+  closeModal.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // feche o modal ao clicar fora dele
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  
